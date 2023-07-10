5f7d9ff781b2ba1b0c8cdbfc7c25d6d3f64ed56a
ViniciusJBSilva <vinicius.josebueno@outlook.com>
2023-07-10 20:28:30 -0300

    Adicionada funcionalidade ao mecanismo de pesquisa, juntamente com a implementação das bibliotecas "requests" e "Marshmallow".
    - Botão de "Atualizar mapa" REMOVIDO!
        - A remoção do botão "Atualizar mapa" ocorreu devido à introdução do novo campo de pesquisa, o qual agora é responsável por executar consultas adicionais.
    
    - Implementação da biblioteca "requests":
        - A biblioteca "requests" foi incorporada ao projeto, pois foram implementadas funções para obter informações por meio de chamadas de API.
    
    -Campo de search para busca de placas.
        - Ao inserir uma placa registrada no sistema da WebRota no campo de pesquisa, é possível obter um rastro de atividades caso exista algum registro correspondente ao período compreendido entre as 00:00 e as 23:59 do dia 03 de julho de 2023.
    
    - Implementação da biblioteca "Marshmallow":
        - Foi realizado a implementação da biblioteca Marshmallow, a qual foi utilizada para serializar as informações obtidas por meio da API da WebRota, permitindo a criação de um arquivo JSON que possibilita a leitura e a posterior plotagem dos dados de rastreamento.
          Além disso, foram adicionadas condições para lidar com diferentes cenários. Caso exista uma placa registrada, mas não haja dados de rastreamento disponíveis, é exibida uma mensagem de erro informando que não há informações de rastreamento para o veículo em questão. Da mesma forma, se uma consulta for realizada para uma placa que não está presente no sistema, será gerado um erro indicando que não foi possível localizar o veículo solicitado.

--- project-ti.py
+++ project-ti.py
@@ -5,10 +5,14 @@ import shutil
 from gmplot import *
 import datetime
 from geopy.distance import geodesic
+import requests
+from marshmallow import Schema, fields
 
 
 app = Flask(__name__, template_folder='templates')
 
+loginwr = "preencher"
+passwordwr = "preencher"
 
 
 db_config = {
@@ -135,6 +139,147 @@ def posi():
     shutil.move(output_file, destination_folder + output_file)
     return render_template("posicao.html")
 
+
+
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    placa = request.form['placa']
+
+    data = {"username":loginwr, "password": passwordwr, "realm": "G"}
+    session = requests.Session()
+    result = session.post('https://api.webrota.com.br/auth/login/', data=data)
+    session.cookies['wrauth-access'] = result.json()['access_token']
+    wr_result = session.get('https://api.webrota.com.br/vehicle/?visible=2&plate='+ placa +'&includes=customer,device,vehicle_model,vehicle_model.vehicle_model_manufacturer,fuel_category,fleet,vehicle_color,vehicle_category,licenses')
+    wr_result = wr_result.json()['data']
+
+    
+
+    if wr_result:
+        id = wr_result[0]['id']
+        return redirect('/posicao/' + str(id))
+    else:
+        novehicle = 'Veículo não encontrado.'
+        return render_template("posicao.html", novehicle=novehicle)
+
+
+
+
+
+class InfoSchema(Schema):
+    vehicle = fields.Str()
+    speed = fields.Int()
+    date_time = fields.DateTime()
+    ignition = fields.Int()
+    latitude = fields.Float()
+    longitude = fields.Float()
+
+
+
+def search2(id):
+    data = {"username": loginwr, "password": passwordwr, "realm": "G"}
+    session = requests.Session()
+    result = session.post('https://api.webrota.com.br/auth/login/', data=data)
+    session.cookies['wrauth-access'] = result.json()['access_token']
+    wr_result = session.get('https://api.webrota.com.br/position/?begin_date=2023-07-03T03:00:00.000Z&end_date=2023-07-04T02:59:00.000Z&vehicles_id=%5B'+ id +'%5D&includes=vehicle,point_of_interest,virtual_area,packet_reason,driver')
+    wr_result = wr_result.json()['data']
+    
+    results = []
+    
+    for result in wr_result:
+        vehicle = result['vehicle']['plate']
+        speed = result['speed']
+        date_time = datetime.datetime.strptime(result['date_time'], "%Y-%m-%dT%H:%M:%S+00:00")
+        ignition = result['ignition_state']
+        latitudefe = result['latitude']
+        longitudefe = result['longitude']
+        
+        results.append({
+            "vehicle": vehicle,
+            "speed": speed,
+            "date_time": date_time,
+            "ignition": ignition,
+            "latitude": latitudefe,
+            "longitude": longitudefe
+        })
+        
+    return results
+
+
+def create_json(id, results):
+    results=search2(id)
+    schema = InfoSchema(many=True)
+    json_output = schema.dumps(results)
+    with open('positionsearch.json', 'w') as f:
+        f.write(json_output)
+
+    return posis()
+    
+
+
+
+
+@app.route('/posicao/<id>', methods=['GET', 'POST'])
+def posi3(id):
+    results = search2(id)
+    return create_json(id, results)
+
+
+
+
+
+
+
+def posis():
+    with open('positionsearch.json') as file:
+        data = json.load(file)
+
+    latitudes = [float(item['latitude']) for item in data]
+    longitudes = [float(item['longitude']) for item in data]
+
+   
+
+    if not latitudes or not longitudes:
+        noresult = "Não existe rastro para este veículo!"
+        return render_template("posicao.html", noresult=noresult)
+    
+   
+
+    gmap = gmplot.GoogleMapPlotter(latitudes[0], longitudes[0], zoom=12)
+    
+    for item in data:
+        ignition_state = item['ignition']
+        ignition_text = "Desligada" if ignition_state == 0 else "Ligada"
+        date_time_utc = item['date_time']
+        date_time_obj = datetime.datetime.fromisoformat(date_time_utc)
+        time_brasilia = datetime.timezone(datetime.timedelta(hours=-3))
+        date_time_brasilia = date_time_obj.astimezone(time_brasilia)
+        formato_date_vehicle_brasilia = date_time_brasilia.strftime("%d/%m/%Y - %H:%M:%S")
+        latitude = float(item['latitude'])
+        longitude = float(item['longitude'])
+        info_box = "<dl><dt style='text-align: center;'><strong>Placa</strong></dt><dd><dt style='text-align: center;'>{}</dt></dd><dt><dt style='text-align: center;'><strong>Ignicao</strong></dt><dd><dt style='text-align: center;'>{}</dt></dd><dt><dt style='text-align: center;'><strong>Velocidade</strong></dt><dd><dt style='text-align: center;'>{} km </dt></dd><dt><dt style='text-align: center;'><strong>Data</strong></dt><dd><dt style='text-align: center;'>{}</dd></dl>".format(item['vehicle'], ignition_text, item['speed'], formato_date_vehicle_brasilia)
+        gmap.marker(latitude, longitude, title=item['vehicle'], info_window=info_box)
+
+    gmap.plot(latitudes, longitudes, 'blue', edge_width=2.5)
+    
+
+    output_file = 'map.html'
+    gmap.draw(output_file)
+
+    destination_folder = 'templates/'
+    shutil.move(output_file, destination_folder + output_file)
+    
+    
+
+
+    return render_template("posicao.html")
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+    
+
+        
+
+
 
